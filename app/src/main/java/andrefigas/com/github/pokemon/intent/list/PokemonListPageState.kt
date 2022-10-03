bc0d1bcdcf616ab0826ad773fe21880bbc6bf285
andrefigas <andre.ce89@gmail.com>
2022-10-03 04:55:47 +0100

    bug fixing

--- app/src/main/java/andrefigas/com/github/pokemon/intent/list/PokemonListPageState.kt
+++ app/src/main/java/andrefigas/com/github/pokemon/intent/list/PokemonListPageState.kt
@@ -5,40 +5,44 @@ import andrefigas.com.github.pokemon.domain.entities.Pokemon
 
 sealed class PokemonListPageState(val pokemons : MutableList<Pokemon>) {
 
-    fun idle() = Idle
-
     object Idle : PokemonListPageState(mutableListOf())
-
-    fun recycled() = Recycled(pokemons)
 
     class Recycled(pokemons : MutableList<Pokemon>) : PokemonListPageState(pokemons)
 
-    fun initialLoading() = InitialLoading(pokemons)
-
     class InitialLoading(pokemons : MutableList<Pokemon>) : PokemonListPageState(pokemons)
-
-    fun incrementalLoading() = IncrementalLoading(pokemons)
 
     class IncrementalLoading(pokemons : MutableList<Pokemon>) : PokemonListPageState(pokemons)
 
-    fun initialFail() = InitialFail(pokemons)
-
     class InitialFail(pokemons : MutableList<Pokemon>) : PokemonListPageState(pokemons)
-
-    fun incrementalFail() = IncrementalFail(pokemons)
 
     class IncrementalFail(pokemons : MutableList<Pokemon>) : PokemonListPageState(pokemons)
 
-    fun initialSuccess(newPokemons : Array<Pokemon>): InitialSuccess {
-        pokemons.addAll(newPokemons)
-        return InitialSuccess(pokemons)
-    }
     class InitialSuccess(pokemons : MutableList<Pokemon>) : PokemonListPageState(pokemons)
+
+    class IncrementalSuccess(pokemons : MutableList<Pokemon>) : PokemonListPageState(pokemons)
+
+    //mappers
 
     fun incrementalSuccess(newPokemons : Array<Pokemon>): IncrementalSuccess {
         pokemons.addAll(newPokemons)
         return IncrementalSuccess(pokemons)
     }
 
-    class IncrementalSuccess(pokemons : MutableList<Pokemon>) : PokemonListPageState(pokemons)
+    fun initialSuccess(newPokemons : Array<Pokemon>): InitialSuccess {
+        pokemons.addAll(newPokemons)
+        return InitialSuccess(pokemons)
+    }
+
+    fun incrementalFail() = IncrementalFail(pokemons)
+
+    fun initialFail() = InitialFail(pokemons)
+
+    fun recycled() = Recycled(pokemons)
+
+    fun incrementalLoading() = IncrementalLoading(pokemons)
+
+    fun initialLoading() = InitialLoading(pokemons)
+
+    fun idle() = Idle
+
 }
